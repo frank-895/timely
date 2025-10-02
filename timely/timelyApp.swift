862d31803f76f0724ca20d3590f4affd2ad36ebc
Frank <frank.snelling03@gmail.com>
2025-10-02 17:20:42 +0100

    feat: add swift and appcast.xml

--- timely/timelyApp.swift
+++ timely/timelyApp.swift
@@ -1,21 +1,25 @@
 import SwiftUI
+import Sparkle
 
 @main
 struct timelyApp: App {
+    // Sparkle updater controller
+    let updaterController = SPUStandardUpdaterController(
+        startingUpdater: true,
+        updaterDelegate: nil,
+        userDriverDelegate: nil
+    )
+
     var body: some Scene {
         MenuBarExtra {
-            // Popover content
             ContentView()
                 .frame(width: 500)
                 .fixedSize(horizontal: false, vertical: true)
         } label: {
-            if let menuIcon = NSImage(named: "MenuIcon") {
-                // Set the proper size on the NSImage itself
-                menuIcon.size = NSSize(width: 16, height: 16)
-                return Image(nsImage: menuIcon)
-            } else {
-                return Image(systemName: "clock")
-            }
+            // Use NSImage if available, else system clock
+            Image(nsImage: NSImage(named: "MenuIcon") ?? NSImage(systemSymbolName: "clock", accessibilityDescription: nil)!)
+                .resizable()
+                .frame(width: 16, height: 16)
         }
         .menuBarExtraStyle(.window)
     }
